136b6e3a85d3749696fe26ffb482681e06a06522
meilizheng <116130097+meilizheng@users.noreply.github.com>
2024-01-22 21:29:50 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GA_SelectionSort_MeiliZheng 
+{
+    internal class Program
+    {
+        static void Main(string[] args)// Selection sort _ MeiliZheng
+        {
+            int[] ints = { 1, 12, 3, 24, 15, 6, 27, 18 };
+            PrintArray(ints);
+
+        }
+
+        static void PrintArray(int[] intArray)
+        {
+            foreach (int i in intArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
