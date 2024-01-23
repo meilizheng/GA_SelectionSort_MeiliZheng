9a83e04df4b80e409b1d4662205f1ede406d6781
meilizheng <116130097+meilizheng@users.noreply.github.com>
2024-01-22 22:10:54 -0800

    Added code

--- Program.cs
+++ Program.cs
@@ -1,20 +1,62 @@
-﻿namespace GA_SelectionSort_MeiliZheng 
-{
-    internal class Program
-    {
-        static void Main(string[] args)// Selection sort _ MeiliZheng
-        {
-            int[] ints = { 1, 12, 3, 24, 15, 6, 27, 18 };
-            PrintArray(ints);
+﻿using System.Diagnostics.Metrics;
 
+namespace GA_SelectionSort_MeiliZheng 
+{
+    public class Program
+    {        
+        static void Main(string[] args)// Selection sort _ MeiliZheng
+        {            
+            int[] ints;
+            SelectionSort(RandomArray());
         }
 
-        static void PrintArray(int[] intArray)
+        static int[] RandomArray()
         {
-            foreach (int i in intArray)
+            Random random = new Random();
+            int minNum = 1;
+            int maxNum = 101;
+            int[] RandomArray = new int[10];
+            for (int i = 0; i < RandomArray.Length; i++)
+            {
+                RandomArray[i] = random.Next(minNum, maxNum);
+            }
+            Console.WriteLine("Unsorted Array:");
+            foreach (int i in RandomArray)
             {
                 Console.WriteLine(i);
             }
+
+            return RandomArray;
         }
+
+        static int[] SelectionSort(int[] intArray)
+        {
+
+            for (int i = 0; i < intArray.Length - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+
+                    if (intArray[j] < intArray[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = intArray[i];
+                intArray[i] = intArray[minIndex];
+                intArray[minIndex] = temp;
+
+            }
+            Console.WriteLine("Sorted Array:");
+            foreach (var i in intArray)
+            {
+                Console.WriteLine(i);
+            }
+            return intArray;
+        }
+
     }
 }
